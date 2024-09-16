b875df49f9f11e7239b1e03f7f97ad47b3ae8337
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-09-16 17:34:29 -0600

    Pila dinamicamente creada, como en las diapositivas.

--- Ejemplos/main.cpp
+++ Ejemplos/main.cpp
@@ -25,12 +25,43 @@ public:
 };
 
 int main(void){
-    Nodo A(5,NULL), B(3,&A), C(7,&B), D(-4,&C);
+    Nodo* tope = NULL;  //Pila vacia
 
-    cout<<"&D = "<<&D<<" "; D.muestraTuEstado();
-    cout<<"&C = "<<&C<<" "; C.muestraTuEstado();
-    cout<<"&B = "<<&B<<" "; B.muestraTuEstado();
-    cout<<"&A = "<<&A<<" "; A.muestraTuEstado();
+    tope = new Nodo(5, tope);    //Push 5
+    tope = new Nodo(3, tope);    //Push 3
+    tope = new Nodo(7, tope);    //Push 7
+    tope = new Nodo(-4, tope);   //Push -4
+
+    Nodo* aux;
+    int d;
+
+    //Pop -4
+    d = tope->dato;
+    aux = tope;
+    tope = tope->inferior;
+    delete aux;
+    cout<<d<<endl;
+
+    //Pop 7
+    d = tope->dato;
+    aux = tope;
+    tope = tope->inferior;
+    delete aux;
+    cout<<d<<endl;
+
+    //Pop 3
+    d = tope->dato;
+    aux = tope;
+    tope = tope->inferior;
+    delete aux;
+    cout<<d<<endl;
+
+    //Pop 5
+    d = tope->dato;
+    aux = tope;
+    tope = tope->inferior;
+    delete aux;
+    cout<<d<<endl;
 
     return 0;
 }
