7efb1918b85893a42eeeb225cc348b8e517ca089
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-09-23 17:26:48 -0600

    Clase Cola y menu

--- Ejemplos/main.cpp
+++ Ejemplos/main.cpp
@@ -2,100 +2,124 @@
 #include <stdlib.h>
 using namespace std;
 
-class PilaChar{
+class Cola{
 private:
-    class Nodo{//La clase Nodo es de uso exclusivo de la clase Pila
+    class Nodo{//La clase Nodo es de uso exclusivo de la clase Cola
     public:
-        char dato;
-        Nodo* inferior;
+        int dato;
+        Nodo* detras;
         Nodo(void){
             dato = 0;
-            inferior = NULL;
+            detras = NULL;
         };
-        Nodo(char d, Nodo* i){
-            dato = d;
-            inferior = i;
+        Nodo(int dat, Nodo* det){
+            dato = dat;
+            detras = det;
         };
         void muestraTuEstado(void){
             cout<<"| "<<dato<<" -> ";
-            if(inferior==NULL)
+            if(detras==NULL)
                 cout << "NULL";
             else
-                cout<< inferior;
+                cout<< detras;
             cout<<" |"<<endl;
         };
     };
-    Nodo* tope;
+    Nodo* primero;
+    Nodo* ultimo;
 public:
-    PilaChar(void){
-        tope = NULL;
+    Cola(void){
+        primero = NULL;
+        ultimo = NULL;
     };
-    ~PilaChar(void){
-        liberaPila();
+    ~Cola(void){
+        liberaCola();
     };
-    void push(char d){
-        tope = new Nodo(d, tope);
+    void push(int dat){
+        if(estaVacia()){
+            primero = new Nodo(dat, NULL);
+            ultimo = primero;
+        }
+        else{
+            ultimo->detras = new Nodo(dat, NULL);
+            ultimo = ultimo->detras;
+        }
     };
-    char pop(void){
-        char d;
+    int pop(void){
+        int d;
         Nodo* aux;
-        d = tope->dato;
-        aux = tope;
-        tope = tope->inferior;
-        delete aux;
+        if(!estaVacia() && primero!=ultimo){
+            d = primero->dato;
+            aux = primero;
+            primero = primero->detras;
+            delete aux;
+        }
+        else if(!estaVacia() && primero==ultimo){
+            d = primero->dato;
+            delete primero;
+            primero = NULL;
+            ultimo = NULL;
+        }
         return d;
     };
     bool estaVacia(void){
-        return tope==NULL;
+        return primero==NULL && ultimo==NULL;
     };
-    void liberaPila(void){
+    void liberaCola(void){
         while(!estaVacia()){
             cout<<pop()<<endl;
         }
     };
 };
 
-bool esNumero(char c);
-bool esMayuscula(char c);
-bool esMinuscula(char c);
-bool esLetra(char c);
-bool esEspecial(char c);
-
 int main(void){
-    string Cadena, Trabajada;
-    char c;
-    int i,tam;
-    cout<<"Ingresa una cadena de caracteres ";
-    getline(cin, Cadena);
-    tam = Cadena.size();
+    Cola C;
+    int op,d;
 
-    PilaChar P;
-    for(i=0; i<tam; i++){
-        if(!esEspecial(Cadena.at(i))){
-            c = toupper(Cadena.at(i));
-            P.push(c);
-            Trabajada.push_back(c);
+    do{
+        system("cls");
+        cout<<"1. push"<<endl
+            <<"2. pop"<<endl
+            <<"3. libera Cola"<<endl
+            <<"4. Salir"<<endl
+            <<"Cual es tu opcion? ";
+        cin>>op;
+        system("cls");
+        switch(op){
+            case 1:
+                cout<<"Ingresa dato ";cin>>d;
+                C.push(d);
+                cout<<endl<<endl<<"Dato insertado en Cola..."<<endl<<endl;
+                break;
+            case 2:
+                if(C.estaVacia()){
+                    cout<<"La Cola ya esta vacia -.-"<<endl<<endl;
+                }
+                else{
+                    d = C.pop();
+                    cout<<"Salio nodo con dato "<<d<<endl<<endl;
+                }
+                break;
+            case 3:
+                if(C.estaVacia()){
+                    cout<<"La Cola ya esta vacia -.-"<<endl<<endl;
+                }
+                else{
+                    C.liberaCola();
+                    cout<<"La Cola ha sido liberada =)"<<endl<<endl;
+                }
+                break;
+            case 4:
+                cout<<"Adios! =)"<<endl<<endl;
+                break;
+            default :
+                cout<<"Opcion invalida! o.O"<<endl<<endl;
+                break;
         }
-    }
-    cout<<endl
-        <<Cadena<<endl<<endl
-        <<Trabajada<<endl<<endl;
-    P.liberaPila();
-    return 0;
-}
+        if(op!=4)
+            system("pause");
 
-bool esNumero(char c){
-    return 48<=c && c <=57;
-}
-bool esMayuscula(char c){
-    return 65<=c && c <=90;
-}
-bool esMinuscula(char c){
-    return 97<=c && c <=122;
-}
-bool esLetra(char c){
-    return esMayuscula(c) || esMinuscula(c);
-}
-bool esEspecial(char c){
-    return !esLetra(c) && !esNumero(c);
+    }while(op!=4);
+
+    return 0;
 }
